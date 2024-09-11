cd9750a5977602d77fc6cdac1d4377c856377875
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-09-11 18:19:16 +0300

    Adds the search user controller

--- backend/Controllers/friendsController.js
+++ backend/Controllers/friendsController.js
@@ -18,7 +18,18 @@ const getMyFriends = async (req, res) => {
     try {
         await connectDB();
         const search_query = req?.query?.name;
-        
+        // search under username, firstname and lastname
+        if (search_query.trim().length === 0) {
+            return res.status(200).json({data: []});
+        }
+        const results = await UserModel.find({$or: [
+            {userName: /search_query/i}, {firstName: /search_query/i}, {lastName: /search_query/i}
+        ]}).select("userName highlight firstName lastName")
+        const data = [];
+        await results.forEach((user) => {
+            data.push(user);
+        })
+        return res.status(200).json({data: data});
     }
     catch(e) {
         console.log(e);
@@ -28,7 +39,7 @@ const getMyFriends = async (req, res) => {
 
   const sendFriendRequest = async (req, res) => {
     try {
-
+        await connectDB();
     }
     catch(e) {
         console.log(e);
