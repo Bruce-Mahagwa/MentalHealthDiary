ee5d6985ca7a5f08504944038a01291505459955
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-09-14 18:24:27 +0300

    Adds content headers for the main content area

--- frontend/src/Components/Headers/ContentHeader.js
+++ frontend/src/Components/Headers/ContentHeader.js
@@ -1,12 +1,44 @@
 // dependencies
-import { Dropdown, Radio, Label, Datepicker } from "flowbite-react";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Button, TextInput } from 'flowbite-react';
+import { FaSearch } from "react-icons/fa";
 const ContentHeader = () => {
     // {title, filter_ribbon, search_bar}
     return (
-        <header>
-            <h3>Write Posts</h3>
+        <header className = "my-4">
+            <h3 className = "text-center">Write Posts</h3>
             {/* navigation for my entries which included filter options */}
+            {/* <nav className = "flex gap-2 flex-wrap md:gap-4 justify-center items-center">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        slotProps={{
+                        textField: {
+                            helperText: 'FROM',
+                        },
+                        }}
+                    />
+                </LocalizationProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        slotProps={{
+                        textField: {
+                            helperText: 'TO',
+                        },
+                        }}
+                    />
+                </LocalizationProvider>
+                <Button color = "success" className = "h-max">Filter Entries</Button>
+            </nav> */}
             {/* end of navigation for my entries */}
+
+            {/* start of navigation for search */}
+            <nav className = "flex gap-2 flex-wrap md:gap-4 justify-center items-center">
+                <TextInput type = "text" className = "w-3/4 sm:w-1/2 xl:w-1/3"/> 
+                <FaSearch className = "cursor-pointer"/>
+            </nav>
+            {/* end of navigation for search */}
         </header>
     )
 }
