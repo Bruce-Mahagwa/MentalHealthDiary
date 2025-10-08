ea6b71d0056f6c7c4de6b07405abd976e261aa4e
Bruce-Mahagwa <jacobbruce880@gmail.com>
2025-10-08 16:55:44 +0300

    Add createdAt field in cookie token

--- backend/Routes/apiRoutes.js
+++ backend/Routes/apiRoutes.js
@@ -14,13 +14,14 @@ const diaryRoutes = require("./diaryEntryRoutes");
 app.get("/get-token", (req, res) => {
   try {
     const accessToken = req.cookies["access_token"];
-    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
-    return res.json({
-      token: {
-        userName: decoded.userName,
-        createdAt: decoded.createdAt
-      }
-    });
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);    
+    // return res.json({
+    //   token: {
+    //     userName: decoded.userName,
+    //     createdAt: decoded.createdAt
+    //   }
+    // });
+    return res.json({ token: decoded });
   }
   catch (e) {
     return res.status(401).json({ error: "Unauthorized User" });
